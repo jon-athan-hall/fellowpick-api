6b5f5092da8f66d14a331f043233b179571c9078
Jonathan Hall <jonathan.edward.hall@gmail.com>
2024-02-20 14:10:42 -0500

    Add one last Deck entity which has many cards.

---
+++ README.md
@@ -0,0 +1,3 @@
+# fellowpick-api
+
+React frontend for users to vote on cutting cards from preconstructed Magic the Gathering decks and adding cards from the corresponding set exclusively.
---
+++ src/main/java/com/fellowpick/api/deck/Deck.java
@@ -0,0 +1,45 @@
+package com.fellowpick.api.deck;
+
+import com.fellowpick.api.card.Card;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Set;
+
+@Entity
+public class Deck {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @OneToMany
+    private Set<Card> cards;
+
+    /**
+     * Constructors.
+     */
+    public Deck() {};
+
+    public Deck(String name, Set<Card> cards) {
+        this.name = name;
+        this.cards = cards;
+    }
+
+    /**
+     * Getters and setters.
+     */
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public Set<Card> getCards() { return cards; }
+
+    public void setCards(Set<Card> cards) { this.cards = cards; }
+}
