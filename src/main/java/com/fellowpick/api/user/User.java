271db632072a338a58cfb9f6f3ce464be44bca36
Jonathan Hall <jonathan.edward.hall@gmail.com>
2024-06-07 16:17:40 -0400

    Create UserRepository and implementations for UserDetails in the User entity.

--- src/main/java/com/fellowpick/api/user/User.java
+++ src/main/java/com/fellowpick/api/user/User.java
@@ -3,17 +3,27 @@ package com.fellowpick.api.user;
 import com.fellowpick.api.pick.Pick;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
     private Long id;
 
+    @Column(unique = true)
     @Email
     private String email;
+
+    @NotNull
+    private String password;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private Set<Pick> picks;
@@ -28,6 +38,43 @@ public class User {
     }
 
     /**
+     * There is required when implementing Spring Security's UserDetails.
+     * @TODO Pretty sure this is an empty list. Return actual authorities.
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    /**
+     * The following implementations are also required for UserDetails.
+     */
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    /**
      * Getters and setters.
      */
     public Long getId() { return id; }
@@ -37,6 +84,10 @@ public class User {
     public String getEmail() { return email; }
 
     public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
 
     public Set<Pick> getPicks() { return picks; }
 
